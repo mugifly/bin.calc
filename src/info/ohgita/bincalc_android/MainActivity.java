96479aea05f9dbfe78d7492ea3ab377a3ca9a527
Masanori Ohgita <mp@ohgita.info>
2012-09-18 21:07:00 +0900

    first commit

---
+++ src/info/ohgita/bincalc_android/MainActivity.java
@@ -0,0 +1,15 @@
+package info.ohgita.bincalc_android;
+
+import com.actionbarsherlock.R;
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
