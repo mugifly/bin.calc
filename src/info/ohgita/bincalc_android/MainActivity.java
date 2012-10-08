1a75e0b7e7dc462fb6f1ad16c54957ad850ef3fc
Masanori Ohgita <piyo3728@gmail.com>
2012-10-08 16:02:26 +0900

    main activity: ActionBar

--- src/info/ohgita/bincalc_android/MainActivity.java
+++ src/info/ohgita/bincalc_android/MainActivity.java
@@ -6,19 +6,36 @@ package info.ohgita.bincalc_android;
  */
 
 import com.actionbarsherlock.R;
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.view.LayoutInflater;
+import android.view.View;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends SherlockFragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);	
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		/* SubMenu button */
+		SubMenu sub_menu = menu.addSubMenu("Overflow Item");
+		sub_menu.getItem().setIcon(R.drawable.ic_action_overflow);
+		sub_menu.getItem().setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		
+		sub_menu.add("Log");
+		
+		/* All clear button*/
+		menu.add("AllClear")
+			.setIcon(R.drawable.button_allclear)
+			.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		return true;
 	}
 }
