07b08df881ed8c2ec9196238ca691c3ce985e542
Masanori Ohgita <piyo3728@gmail.com>
2012-10-12 22:46:41 +0900

    appInfo Activity

--- src/info/ohgita/bincalc_android/MainActivity.java
+++ src/info/ohgita/bincalc_android/MainActivity.java
@@ -11,14 +11,21 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.SubMenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends SherlockFragmentActivity {
+	
+	final static int MENU_ID_LOG = 100;
+	final static int MENU_ID_ABOUT = 200;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 		setContentView(R.layout.activity_main);
 		
 	}
@@ -32,10 +39,32 @@ public class MainActivity extends SherlockFragmentActivity {
 		
 		sub_menu.add("Log");
 		
+		sub_menu.add(Menu.NONE, MENU_ID_ABOUT, Menu.NONE , R.string.menu_about);
+		
 		/* All clear button*/
 		menu.add("AllClear")
 			.setIcon(R.drawable.button_allclear)
 			.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 		return true;
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		boolean ret = true;
+		switch (item.getItemId()) {
+		default:
+			ret = super.onOptionsItemSelected(item);
+			break;
+		case MENU_ID_LOG:
+		    ret = true;
+		    break;
+		case MENU_ID_ABOUT:
+		    ret = false;
+		    Intent intent = new Intent(MainActivity.this,Activity_appInfo.class);
+		    intent.setAction(Intent.ACTION_VIEW);
+		    startActivity(intent);
+		    break;
+		}
+		return ret;
 	}
 }
