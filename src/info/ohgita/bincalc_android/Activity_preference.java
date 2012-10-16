6fc4bcb3df17a4116d21407738d0afd0acd06532
Masanori Ohgita <piyo3728@gmail.com>
2012-10-16 09:13:14 +0900

    preference activity: fixed theme

--- src/info/ohgita/bincalc_android/Activity_preference.java
+++ src/info/ohgita/bincalc_android/Activity_preference.java
@@ -1,19 +1,21 @@
 package info.ohgita.bincalc_android;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import com.actionbarsherlock.R;
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 public class Activity_preference extends SherlockPreferenceActivity {
+	@SuppressLint("NewApi")
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
 		addPreferencesFromResource(R.xml.preference);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	
 	@Override  
