9242e5d9b9f0709da031420b48f2c8a1326418d4
Masanori Ohgita <piyo3728@gmail.com>
2012-10-20 22:59:43 +0900

    BasicArithOperator: cleanup

--- src/info/ohgita/bincalc_android/calc/BasicArithOperator.java
+++ src/info/ohgita/bincalc_android/calc/BasicArithOperator.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import android.util.Log;
 
 /**
- * Basic arithmetic operation class
+ * Basic arithmetic operation class for bin.Calc
  * @author masanori
  * 
  */
@@ -21,87 +21,57 @@ public class BasicArithOperator {
 	
 	/**
 	 * calculation
-	 * @param (LinkedList list) 
-	 * @return (String) resultant value
+	 * @param list LinkedList 
+	 * @return resultant value String
 	 */
 	public String calculation(LinkedList<String> linkedList){
 		list = linkedList;
-		Log.i("binCalc","calculation(...) "+list.toString());
+		Log.i("BasicArithOperator","calculation(...) "+list.toString());
 		return eval(list.iterator());
 	}
 	
+	/**
+	 * eval (recursive method)
+	 * @param iter LinkedList iterator
+	 * @return resultant value String
+	 */
 	protected String eval(Iterator<String> iter){
-		Log.i("binCalc", " eval(...)");
+		Log.i("BasicArithOperator", " eval(...)");
 		int i = 0;
 		double result = 0;
 		String beforeStr = "";
 		while(iter.hasNext()){
 			String str = iter.next();
-			Log.i("binCalc","  * str: "+str + " - result = "+result);
+			Log.i("BasicArithOperator","  * str: "+str + " - result = "+result);
 			if(str.contentEquals("(")){
 				Iterator<String> it = iter;
 				str = eval(it);
 			}else if(str.contentEquals(")")){
-				Log.i("binCalc","  * break");
+				Log.i("BasicArithOperator","  * break");
 				break;
 			}
 			
 			if(beforeStr.contentEquals("+")){
-				Log.i("binCalc","    * "+result+" += "+str);
+				Log.i("BasicArithOperator","    * "+result+" += "+str);
 				result += Double.parseDouble(str);
 			}else if(beforeStr.contentEquals("-")){
-				Log.i("binCalc","    * "+result+" -= "+str);
+				Log.i("BasicArithOperator","    * "+result+" -= "+str);
 				result -= Double.parseDouble(str);
 			}else if(beforeStr.contentEquals("*")){
-				Log.i("binCalc","    * "+result+" *= "+str);
+				Log.i("BasicArithOperator","    * "+result+" *= "+str);
 				result *= Double.parseDouble(str);
 			}else if(beforeStr.contentEquals("/")){
-				Log.i("binCalc","    * "+result+" /= "+str);
+				Log.i("BasicArithOperator","    * "+result+" /= "+str);
 				result /= Double.parseDouble(str);
 			}else if(beforeStr.length() == 0){
-				Log.i("binCalc","    * "+result+" = "+str);
+				Log.i("BasicArithOperator","    * "+result+" = "+str);
 				result = Double.parseDouble(str);
 			}
 			iter.remove();
 			beforeStr = str;
 			i++;
 		}
-		Log.i("binCalc", " * return("+result+")");
+		Log.i("BasicArithOperator", " * return("+result+")");
 		return Double.toString(result);
 	}
-	
-	/**
-	 * Replace after part of iterator of the LinkedList, with character string
-	 * @param queue
-	 * @param iter
-	 * @param replaceStr
-	 * @return replaced queue
-	 * 
-	 */
-	protected LinkedList<String> replaceRange(LinkedList<String> queue, Iterator<String> iter, String replaceStr){
-		LinkedList<String> newLinkedList = new LinkedList<String>();
-		while(iter.hasNext()){
-			String str = iter.next();
-			if(str.contentEquals(")")){
-				break;
-			}
-		}
-		return queue;
-	}
-	
-	/**
-	 * Clip out LinkedList, only part after iterator.
-	 * @param queue
-	 * @param iter
-	 * @return clipped queue
-	 */
-	protected LinkedList<String> clipRange(LinkedList<String> queue, Iterator<String> iter){
-		LinkedList<String> newList = new LinkedList<String>();
-		while(iter.hasNext()){
-			String str = iter.next();
-			newList.addLast(str);
-		}
-		return newList;
-	}
-	
 }
